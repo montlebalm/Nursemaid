7fa9edcf4f9cf0f1cd2f7ad12acf4500973ccc80
Chris Montrois <cmontrois@quickleft.com>
2015-02-18 22:55:04 -0700

    Adding 'Timer'

--- Nursemaid/Nursemaid/Timer.swift
+++ Nursemaid/Nursemaid/Timer.swift
@@ -1,9 +1,37 @@
-//
-//  Timer.swift
-//  Nursemaid
-//
-//  Created by Chris Montrois on 2/18/15.
-//  Copyright (c) 2015 bigevilrobot. All rights reserved.
-//
-
 import Foundation
+
+public class Timer {
+
+  public var seconds = 0
+
+  var timer: NSTimer
+  var onTick: () -> ()
+
+  public init(update: (() -> ())? = nil) {
+    onTick = update != nil ? update! : {}
+    timer = NSTimer()
+  }
+
+  public func start() {
+    stop()
+    timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick", userInfo: nil, repeats: true)
+  }
+
+  public func stop() {
+    timer.invalidate()
+  }
+
+  public func running() -> Bool {
+    return timer.valid
+  }
+
+  @objc func tick() {
+    seconds += 1
+    onTick()
+  }
+
+  deinit {
+    stop()
+  }
+
+}
--- Nursemaid/NursemaidTests/TimerTests.swift
+++ Nursemaid/NursemaidTests/TimerTests.swift
@@ -1,9 +1,63 @@
-//
-//  TimerTests.swift
-//  Nursemaid
-//
-//  Created by Chris Montrois on 2/18/15.
-//  Copyright (c) 2015 bigevilrobot. All rights reserved.
-//
+import XCTest
+import Nursemaid
 
-import Foundation
+class TimerTests: XCTestCase {
+
+  // Seconds
+
+  func testSecondsStartAt0() {
+    let t = Timer()
+    XCTAssertEqual(t.seconds, 0)
+  }
+
+  // Running
+
+  func testRunningOnInit() {
+    let timer = Timer()
+    XCTAssertFalse(timer.running())
+  }
+
+  func testRunningWhenStarted() {
+    let expect = expectationWithDescription("Waiting for tick")
+
+    let timer = Timer() {
+      expect.fulfill()
+    }
+    timer.start()
+
+    waitForExpectationsWithTimeout(5) { err in
+      XCTAssert(timer.running())
+      timer.stop()
+    }
+  }
+
+  func testRunningWhenStopped() {
+    let expect = expectationWithDescription("Waiting for tick")
+    let timer = Timer() {
+      expect.fulfill()
+    }
+    timer.start()
+
+    waitForExpectationsWithTimeout(5) { err in
+      timer.stop()
+      XCTAssertFalse(timer.running())
+    }
+  }
+
+  // Update
+
+  func testUpdateIncrementsSeconds() {
+    let expect = expectationWithDescription("Waiting for tick")
+
+    let timer = Timer() {
+      expect.fulfill()
+    }
+    timer.start()
+
+    waitForExpectationsWithTimeout(5) { err in
+      timer.stop()
+      XCTAssertEqual(timer.seconds, 1)
+    }
+  }
+
+}
